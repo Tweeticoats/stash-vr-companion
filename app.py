e61b93bd71e816c1dddebfba02562f860bb01849
Tweeticoats <Tweeticoats@github.com>
2021-12-13 09:30:38 +1030

    changing vr filter back to export_deovr and SBS.

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ def filter():
 
     vr_filter ={}
     vr_filter['name']='VR'
-    vr_filter['filter']={"tags": {"value": [tags_cache['export_deovr']['id'],tags_cache['SBS']['id']], "depth": 0, "modifier": "INCLUDES"}}
+    vr_filter['filter']={"tags": {"value": [tags_cache['export_deovr']['id'],tags_cache['SBS']['id']], "depth": 0, "modifier": "INCLUDES_ALL"}}
 
     flat_filter={}
     flat_filter['name']='2D'
